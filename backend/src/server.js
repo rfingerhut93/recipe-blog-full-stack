7fc960e2e83532a7ec530ca9ad1ad6716cc52809
Rachel Fingerhut <fingerhutrachel@gmail.com>
2023-09-08 14:54:38 -0700

    Connects api requests to MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,78 +1,109 @@
 import express from 'express';
-
+import { db, connectToDb } from './db.js'
 const app = express();
 app.use(express.json());
 
-let fakeRecipeData = [
-    {
-        name: "gyros",
-        title: "Gryos",
-        ingredients: [
-            { name: 'Lamb', measurement: '3 lb'},
-            { name: 'Bacon', measurement: '4 strips'},
-            { name: 'Onion', measurement: '1'}
-        ],
-        directions: "Make it"
-    },
-    {
-        name: "spaghetti-and-meatballs",
-        title: "Spaghetti and Meatballs",
-        ingredients: [
-            { name: 'Spaghetti', measurement: '1 box' },
-            { name: 'Ground Beef', measurement: '0.5 pound'},
-            { name: 'Ground Pork', measurement: '0.5 pound'}
-        ],
-        directions: "Make it"
-    }
-]
 
-// retrieves existing recipe
-app.get('/api/recipes/:name', (req,res) => {
+// endpoint to retrieve data from MongoDB database
+app.get('/api/recipes/:name', async (req,res) => {
     const {name} = req.params;
-    const recipe = fakeRecipeData.find(recipe => recipe.name === name);
+
+
+    const recipe = await db.collection('recipes').findOne({ name });
     if (recipe){
-        res.send(recipe);
+        res.json(recipe);
     } else {
-        res.send('That recipe doesn\'t exist.');
+        res.sendStatus(404);
     }
-});
+})
 
 // adds new recipe
 app.post('/api/recipes', async (req, res) => {
+    console.log(req.body);
+
     const newRecipe = req.body;
-    const existingRecipe = fakeRecipeData.find((recipe => recipe.name === newRecipe.name));
+    const {name} = newRecipe.name;
+
+    const existingRecipe = await db.collection('recipes').findOne({name});
 
     if (existingRecipe){
-        return res.status(400).json({error: 'Recipe with same name already exists. Either update existing recipe or use a different name.'});
-    }
+        return res.status(400).json({error: `Recipe with same name (${newRecipe.name}) already exists. Either update existing recipe or use a different name.`});
+    } 
 
-    fakeRecipeData.push(newRecipe);
-
+    db.collection('recipes').insertOne(newRecipe);
     res.status(201).json({message: "Recipe added successfuly", recipe: newRecipe});
 });
 
-// updates existing recipe (BY REPLACEMENT)
-app.put('/api/recipes/:name', (req, res) => {
-    const {name } = req.params;
+// updates existing recipe's ingredients by adding on.
+app.put('/api/recipes/:name/ingredients', async (req, res) => {
+    const { name } = req.params;
+    const ingredient = req.body;
 
-    const existingRecipe = fakeRecipeData.find(recipe => recipe.name === name);
 
-    if (existingRecipe) {
-        if (req.body.ingredients) {
-            existingRecipe.ingredients = req.body.ingredients;
+    const existingRecipe = await db.collection('recipes').findOne({ name });
+
+    if (existingRecipe){
+        const ingredientExists = existingRecipe.ingredients.some((existingIngredient) => {
+            return (
+                existingIngredient.name === ingredient.name && existingIngredient.measurement === ingredient.measurement
+            );
+        });
+
+        if (!ingredientExists){
+            const updatedRecipe = await db.collection('recipes').updateOne({ name }, {
+                $push: {ingredients: ingredient}}
+            );
+            if (updatedRecipe){
+                res.send('Recipe updated successfully');
+            } else{
+                res.send('Error: Recipe was not updated.');
+            }
+        } else {
+            res.send('That ingredient and measurement are already added to the ingredients list.');
         }
-    
-        if (req.body.directions) {
-            existingRecipe.directions = req.body.directions;
-        }
-        res.send('Recipe updated successfully');
+
     } else{
         res.send('That recipe doesn\'t exist.');
     }
-
 });
 
-app.listen(8000, () => {
-    console.log('Server is listening to port 8000');
+// updates existing recipe's directions by updating specific index of directions array, or will add a new element if no index is specified.
+app.put('/api/recipes/:name/directions', async(req, res)=> {
+    const { name } = req.params;
+    const { index, newDirection } = req.body;
+
+
+
+    const existingRecipe = await db.collection('recipes').findOne({ name });
+
+    if (existingRecipe){
+
+        if (index !== null){
+            existingRecipe.directions[index] = newDirection;
+            const updatedRecipe = await db.collection('recipes').updateOne({ name }, {
+                $set: {directions: existingRecipe.directions}
+            });
+
+            if (updatedRecipe){
+                res.send('Recipe\'s directions updated successfully');
+            } else {
+                res.send('Error: Recipe not updated.');
+            }
+
+        } else {
+            res.send('Error: Please enter an index.')
+        }
+
+    } else {
+        res.send('Error: This recipe does not exist and cannot be updated.')
+    }
 });
+
+
+connectToDb(() => {
+    console.log('Successfully connected to database.')
+    app.listen(8000, () => {
+        console.log('Server is listening to port 8000');
+    });
+})
 
